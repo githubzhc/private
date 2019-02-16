79ae18be86b533e91c796de8c890b41afa659ea4
chichi <zhanghanchi123@139.com>
2019-02-16 15:03:49 +0800

    zhc

---
+++ 2-OOP/01.py
@@ -0,0 +1,14 @@
+'''
+学生类
+
+'''
+class Student():
+    name=None
+    age=19
+    def dohomework(self):
+        print("在做作业")
+
+mingyue=Student()
+print(mingyue.age)
+mingyue.dohomework()
+print(mingyue)
